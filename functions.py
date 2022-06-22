f2f15e6dbbbdb9c74534fe2339f708c0f7c12d46
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-06-22 15:01:04 +0700

    code improovement type hinting added

--- functions.py
+++ functions.py
@@ -17,12 +17,19 @@ import random
 # ```
 
 
-def init_board(board_size=3):
-    board = []
+def init_board(board_size: int = 3, empty_field: str = '.') -> list:
+    """ function to return board in size of board_size nested, with empty field
+
+    :param empty_field: (str) def '.'
+    :param board_size: (int) size of elements in the same number of nested lists
+    :return: board - board size number of nested lists with board size of elements
+    """
+
+    board = []  # TO DO list comprehension
     for list_lvl0 in range(board_size):
         board.append([])
         for list_lvl1 in range(board_size):
-            board[list_lvl0].append('.')
+            board[list_lvl0].append(empty_field)
     return board
 
 
@@ -32,7 +39,7 @@ def init_board(board_size=3):
 #       first column, etc.
 #     - The function returns a tuple of two integers: (row, col)
 # ??    - The returned coordinates start from 0
-#     - The integers indicate a valid (empty) position on the board
+#     - The integers indicate a valid (emptyemplty_field) position on the board
 
 #     - If the user provides coordinates that are outside of board, keep asking
 #     - If the user provides coordinates for a place that is taken, keep asking
@@ -41,50 +48,76 @@ def init_board(board_size=3):
 #     - When the player types `quit` instead of coordinates, the program exits.
 # get_move fun added
 
-# coordinates = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
 
+def quit_game(exit_message: str = 'Bye Bye', delay: float = 1) -> None:
+    """ function to exit app when called with custom message, after delayed time
 
-def quit_game():
-    print('Bye Bye')
-    time.sleep(1)
+    :param exit_message: (str) custom message to print when before exit, def 'Bye Bye'
+    :param delay: (float) time seconds to delay exit
+    """
+    print(exit_message)
+    time.sleep(delay)
     sys.exit()
 
 
-def available_moves(boards):
+def available_moves(boards: list, empty_cell: str = '.') -> list:
+    """function to return list of the available cells - one marks with empty_cell (str)
+    on boards(nestedlist)
+
+    :param empty_cell: (str) represents empty cell in boards)
+    :param boards: list nested list of cells that is board for a game
+    :return: list of available moves
+    """
     available_moves_list = []
-    for i_list in range(len(boards)):
+    for i_list in range(len(boards)):  # TO DO list comprehension
         for i_element in range(len(boards[i_list])):
-            if boards[i_list][i_element] == '.':
+            if boards[i_list][i_element] == empty_cell:
                 available_moves_list.append((i_list, i_element))
     return available_moves_list
 
 
-def translate_input_moves(inp_str):
+def translate_input_moves(inp_str: str) -> tuple:
+    """returns tuple of 2 numbers from a string, its a translation of
+    coordinates input by user in form a1 b2 c3 etc...
+
+    :param inp_str: user input of coordinates
+    :return: tuple of two ints which are coordinates
+    """
+    # correct_inp_str_list = ['a', 'b', 'c']
     if (tuple(inp_str)[0]) == 'a':
         return tuple((0, (int((tuple(inp_str)[1]))) - 1))
-    elif (tuple(inp_str)[0]) == 'b':
+
+    if (tuple(inp_str)[0]) == 'b':
         return tuple((1, (int((tuple(inp_str)[1]))) - 1))
-    elif (tuple(inp_str)[0]) == 'c':
+
+    if (tuple(inp_str)[0]) == 'c':
         return tuple((2, (int((tuple(inp_str)[1]))) - 1))
-    else:
-        return None
+    # if (tuple(inp_str)[0]) not in correct_inp_str_list and (int((tuple(inp_str)[1]))) not in range(4):
+    # #    return None         # call error! this is where incorrect input is coming from
 
 
-def get_move(boards):
+def get_move(boards: list) -> tuple:
     """function returns tuple with coordinates (row, column) from user input."""
 
     incorrect_input = True
+    correct_move = [['a', 'b', 'c'], ['1', '2', '3']]
     while incorrect_input:
         move = input("Choose field by providing coordinates f.e.: A2:").casefold()
+
         if move == 'Quit'.casefold():
             quit_game()
+        elif move[0] not in correct_move[0] or move[1] not in correct_move[1] or len(move) != 2:
+            print(f'Wrong Input. Available moves:{available_moves(boards)}')
         elif translate_input_moves(move) in available_moves(boards):
             return translate_input_moves(move)
-        else:
-            pass
 
 
-def get_move_ai(boards):        # not real AI computer just pick random
+def get_move_ai(boards: list) -> tuple:  # not real AI computer just pick random
+    """function to randomly pick and return in form of tuple a move from available moves"
+
+    :param boards: list of boards
+    :return: tuple of coordinates
+    """
     move = random.choice(available_moves(boards))
     time.sleep(0.8)
     return move
@@ -96,30 +129,36 @@ def get_move_ai(boards):        # not real AI computer just pick random
 #     - It does not do anything if the cell is already marked
 
 
-def mark(player, boards, row, col):
-    """function marks player X or 0 if the space is available
+def mark(player: int, boards: list, row: int, col: int) -> None:
+    """function to mark player move on boards
 
-    player - Player1 - X, Player2 - 0
-    boards - board where to make a mark
-    row - row of boards
-    col - column of boards
+    :type boards: list
+    :param player: int 1 or 2
+    :param boards: list of boards where to make a mark
+    :param row: int index of list in boards
+    :param col: int index of element in boards
     """
-
     if player == 0:
         player_mark = 'X'
     else:
         player_mark = '0'
-    if boards[row][col] == '.':
-        boards[row][col] = player_mark
+
+    boards[row][col] = player_mark
 
 
 # 4. Implement `has_won()` that returns `True` if `player` (`X` or `0`)  has three of their marks in a horizontal,
 #    vertical, or diagonal row on `board`.
 #     - Returns `True` if `player` has a three-in-a-row on `board`.
-#     - Returns `False` if `player` doesn't have a three-in-a-row on `board`
+#     - Returns `False` if `player` doesn't have a three-in-a-row on `board`li
 
 
-def has_won(player, boards):  # to be PYTHONIC
+def has_won(player: str, boards: list) -> bool:     # TO DO pythonic
+    """function to return True for if win condition is reached
+
+    :param player: str 'X' or '0'
+    :param boards: list of boards of the game
+    :return: True if player has correct cells crossed
+    """
     if boards[0][0] == boards[0][1] == boards[0][2] == player or \
             boards[1][0] == boards[1][1] == boards[1][2] == player or \
             boards[2][0] == boards[2][1] == boards[2][2] == player or \
@@ -129,19 +168,22 @@ def has_won(player, boards):  # to be PYTHONIC
             boards[0][0] == boards[1][1] == boards[2][2] == player or \
             boards[2][0] == boards[1][1] == boards[0][2] == player:
         return True
-    else:
-        return False  # i tried all method but didn't work:
-#     all(boards[0], player) - did return ture all the time...
 
 
 # 5. Implement `is_full()` that returns `True` if the board is full.
 #     - Returns `True` if there are no empty fields on the board
 #     - Returns `False` otherwise
-def is_full(lists):
-    if any('.' in nested_list for nested_list in lists):
+def is_full(lists: list, empty_cell: str = '.') -> bool:
+    """function return false if boards have still available moves
+
+    :param empty_cell: str def '.' how the empty_cell are marked
+    :param lists:
+    :return:
+    """
+    if any(empty_cell in nested_list for nested_list in lists):
         return False
-    else:
-        return True
+    # else:
+    #     return True
 
 
 # 6.DONE Implement `print_board()` that prints the board to the screen.
@@ -159,7 +201,12 @@ def is_full(lists):
 # ```
 
 
-def print_board(boards):
+def print_board(boards: list) -> None:
+    """function to print on-screen boards of game
+
+    :param boards: lists of game cells
+    """
+
     print()
     print(f'\t1   2   3\nA\t', end='')
     print(*boards[0], sep=' | ')
@@ -176,7 +223,12 @@ def print_board(boards):
 #     - If nobody wins, print "It's a tie!"
 
 
-def print_result(boards):
+def print_result(boards: list) -> None:
+    """function to print winner or draw on-screen
+
+    :param boards: lists of boards
+    """
+
     if has_won('X', boards) is True:
         print('X has won!')
     elif has_won('0', boards) is True:
@@ -192,9 +244,10 @@ def print_result(boards):
 #     - The game ends when someone wins or the board is full
 #     - The game handles bad input (wrong coordinates) without crashing
 
-def tictactoe_game(mode='Players'):
-    """fun to call the game
-    mode: define mode to be played: Players, Human-Ai, Ai-Human, Ai-Ai
+def tictactoe_game(mode: str = 'Players') -> None:
+    """function to call the game in mode:str
+
+    :param mode: str 'Players', 'Human-Ai', 'Ai-Human', 'Ai-Ai'
     """
     os.system('clear')
     boards = init_board()
@@ -257,17 +310,13 @@ def main_manu():
             pass
 
 
-# 10.DONE  Implement player-against-AI mode. The AI can drive one of the players, and the game is fully playable against the
-#     computer.
-#     - When `tictactoe_game()` is called with the argument `'HUMAN-AI'` then it calls `get_ai_move()` instead
-#       of `get_move()` when it's Player `0` turn
-#     - When `tictactoe_game()` is called with the argument `'AI-HUMAN'` then it calls `get_ai_move()` instead
-#       of `get_move()` when it's Player `X` turn
-#     - Function `get_ai_move()` returns a valid move (if possible) without asking for any input
-#     - Function `get_ai_move()` returns `None` if board is full
-#     - Function `main_menu()` is implemented as a menu for between choosing 2-player
-#       mode and against-AI mode by pressing
-#       1 or 2, respectively
+# 10.DONE  Implement player-against-AI mode. The AI can drive one of the players, and the game is fully playable
+# against the computer. - When `tictactoe_game()` is called with the argument `'HUMAN-AI'` then it calls
+# `get_ai_move()` instead of `get_move()` when it's Player `0` turn - When `tictactoe_game()` is called with the
+# argument `'AI-HUMAN'` then it calls `get_ai_move()` instead of `get_move()` when it's Player `X` turn - Function
+# `get_ai_move()` returns a valid move (if possible) without asking for any input - Function `get_ai_move()` returns
+# `None` if board is full - Function `main_menu()` is implemented as a menu for between choosing 2-player mode and
+# against-AI mode by pressing 1 or 2, respectively
 #
 # 11. [OPTIONAL] AI is capable of recognizing the opportunity to win the game with one move.
 #     - Function `get_ai_move()` picks the winning move if there is one on the board
@@ -288,6 +337,3 @@ def main_manu():
 #     - When `tictactoe_game()` is called with the argument `'AI-AI'` then it calls `get_ai_move` for both players
 #     - The game comes to an end without any user input
 #     - Game play is easy to follow as there is a 1 seconds delay between the moves
-
-
-
